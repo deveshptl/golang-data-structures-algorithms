ef23868bdb5d1ea3ffb34df96fc048a5d9c98d28
computer0101 <deveshpatel7643@gmail.com>
2018-10-13 15:02:33 +0530

    add comments in stack.go file

--- stack/stack.go
+++ stack/stack.go
@@ -2,63 +2,94 @@ package main
 
 import "fmt"
 
-var len int
+var stack [5]string
+var size, top int
+
+func init() {
+	for i := 0; i < len(stack); i++ {
+		stack[i] = "nil"
+	}
+}
 
 func main() {
-	len = 5
-	top := -1
-	x := make([]string, 6)
-	selection := 1
-	for selection == 1 {
+	size = 4
+	top = -1
+	i := 0
+	for i == 0 {
 		var choice int
 		fmt.Println("\n1. PUSH")
 		fmt.Println("2. POP")
-		fmt.Println("3. EXIT")
+		fmt.Println("3. DISPLAY")
+		fmt.Println("4. EXIT")
 		fmt.Print("\nEnter you choice: ")
 		fmt.Scanf("%d", &choice)
 
 		switch choice {
 		case 1:
-			x, top = push(x, top)
-			fmt.Println("\nValues after PUSH ", x, top)
+			push()
+			fmt.Println("\nValues after push TOP:", top)
+			display()
 			break
 		case 2:
-			x, top = pop(x, top)
-			fmt.Println("\nValues after POP ", x, top)
+			pop()
+			fmt.Println("\nValues after pop TOP:", top)
+			display()
 			break
 		case 3:
-			selection = 0
+			fmt.Println("\nTOP:", top)
+			display()
+		case 4:
+			i = 1
 			break
 		default:
 			fmt.Println("Choice not recognized")
 		}
 
 	}
-
-	fmt.Println("Final values: ", x, top)
-	fmt.Println("Exited...")
-
 }
 
-func push(x []string, top int) ([]string, int) {
-	if len == top {
-		fmt.Println("Stack overflow")
-		return x, top
+func push() {
+
+	// check for overflow
+	if size == top {
+		fmt.Println("\n-- Stack overflow --")
+		return
 	}
+
+	// scan for element
 	var val string
 	fmt.Print("Enter the value that you want to PUSH: ")
 	fmt.Scanf("%s", &val)
+
+	// increment top pointer
 	top = top + 1
-	x[top] = val
-	return x, top
+
+	// insert the element
+	stack[top] = val
 }
 
-func pop(x []string, top int) ([]string, int) {
+func pop() {
+
+	// check for underflow
 	if top == -1 {
-		fmt.Println("Stack underflow")
-		return x, top
+		fmt.Println("\n-- Stack underflow --")
+		return
 	}
 
+	// Print popped value
+	y := stack[top]
+	fmt.Println("\nValue popped:", y)
+
+	// initialize to nil
+	stack[top] = "nil"
+
+	// decrement top pointer
 	top = top - 1
-	return x[:top+1], top
+}
+
+func display() {
+	for i := 0; i < len(stack); i++ {
+		fmt.Print(stack[i], " ")
+	}
+	fmt.Println("")
 }
