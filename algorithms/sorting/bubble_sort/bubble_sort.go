4b1fcf19c8decfdbdfb6e6139fa521f92a61735b
computer0101 <deveshpatel7643@gmail.com>
2018-12-24 17:47:22 +0530

    implement bubble sort

---
+++ algorithms/sorting/bubble_sort/bubble_sort.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+var arr []int
+
+func main() {
+	var size int
+	var element int
+	fmt.Println("\n-- Bubble Sort --")
+	fmt.Print("\nEnter the size of an array: ")
+	fmt.Scanf("%d", &size)
+	fmt.Println("Start entering elements:")
+	for i := 0; i < size; i++ {
+		fmt.Scanf("%d", &element)
+		arr = append(arr, element)
+	}
+
+	// sorting
+	for i := size - 1; i >= 0; i-- {
+		for j := 0; j < i; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+
+	fmt.Println("Sorted Array is:")
+	fmt.Println(arr)
+}
