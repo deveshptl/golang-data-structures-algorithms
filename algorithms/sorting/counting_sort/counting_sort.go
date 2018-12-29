803a798f7fe7b8916b3157b28336b4f7d56a0e97
computer0101 <deveshpatel7643@gmail.com>
2018-12-29 18:53:16 +0530

    implement counting sort

---
+++ algorithms/sorting/counting_sort/counting_sort.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func countingSort(size, max int) []int {
+	count := make([]int, max)
+	sorted := make([]int, size)
+	for i := 0; i < size; i++ {
+		count[arr[i]] = count[arr[i]] + 1
+	}
+	for i := 1; i < max; i++ {
+		count[i] = count[i] + count[i-1]
+	}
+	for i := size - 1; i >= 0; i-- {
+		sorted[count[arr[i]]-1] = arr[i]
+		count[arr[i]] = count[arr[i]] - 1
+	}
+	return sorted
+}
+
+var arr []int
+
+func main() {
+	var size, element, min, max int
+	fmt.Println("\n-- Counting Sort --")
+	fmt.Print("\nEnter the size of an array: ")
+	fmt.Scanf("%d", &size)
+	fmt.Println("Start entering elements:")
+	for i := 0; i < size; i++ {
+		fmt.Scanf("%d", &element)
+
+		if i == 0 {
+			min = element
+			max = element
+		}
+
+		min = int(math.Min(float64(min), float64(element)))
+		max = int(math.Max(float64(max), float64(element)))
+		arr = append(arr, element)
+	}
+	sorted := countingSort(size, max+1)
+	fmt.Println("Sorted array is:")
+	fmt.Println(sorted)
+}
