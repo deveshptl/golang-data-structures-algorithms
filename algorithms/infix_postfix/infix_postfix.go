02695084c07cd1d6626cd6727106246963a149e3
computer0101 <deveshpatel7643@gmail.com>
2019-01-01 17:51:00 +0530

    implement infix to postfix conversion

---
+++ algorithms/infix_postfix/infix_postfix.go
@@ -0,0 +1,72 @@
+package main
+
+import "fmt"
+
+// InToPos converts an infix expression to postfix expression
+func InToPos() {
+	postfix := ""
+	stack = append(stack, "N")
+	for i := range expression {
+		char := string(expression[i])
+		if (expression[i] >= 65 && expression[i] <= 90) || (expression[i] >= 97 && expression[i] <= 122) {
+			postfix += char
+		} else if char == "(" {
+			stack = append(stack, char)
+		} else if char == ")" {
+			for stack[len(stack)-1] != "N" && stack[len(stack)-1] != "(" {
+				newChar := stack[len(stack)-1]
+				stack = stack[0 : len(stack)-1]
+				postfix += newChar
+			}
+			if stack[len(stack)-1] == "(" {
+				stack = stack[0 : len(stack)-1]
+			}
+		} else {
+			for stack[len(stack)-1] != "N" && Prec(stack[len(stack)-1]) >= Prec(char) {
+				newChar := stack[len(stack)-1]
+				stack = stack[0 : len(stack)-1]
+				postfix += newChar
+			}
+			stack = append(stack, char)
+		}
+	}
+	for stack[len(stack)-1] != "N" {
+		newChar := stack[len(stack)-1]
+		stack = stack[0 : len(stack)-1]
+		postfix += newChar
+	}
+	fmt.Println("\n" + postfix)
+}
+
+var stack []string
+var expression string
+
+func main() {
+	fmt.Println("\n-- Infix to Postfix expression conversion --")
+	i := 0
+	for i == 0 {
+		fmt.Print("\nEnter the expression: ")
+		fmt.Scanf("%s", &expression)
+		InToPos()
+		var choice string
+		fmt.Print("\nDo you want to continue y/n? ")
+		fmt.Scanf("%s", &choice)
+		if choice == "N" || choice == "n" {
+			break
+		} else {
+			fmt.Println("Command not recognized.")
+		}
+	}
+}
+
+// Prec will return a precedence of a given operator
+func Prec(c string) int {
+	if c == "^" {
+		return 3
+	} else if c == "*" || c == "/" {
+		return 2
+	} else if c == "+" || c == "-" {
+		return 1
+	}
+	return -1
+}
