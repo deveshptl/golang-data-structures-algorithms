7488d932254d81c92f9373459c4a063573f61165
computer0101 <deveshpatel7643@gmail.com>
2019-01-07 06:57:28 +0530

    implement union find algorithm

---
+++ algorithms/union_find/graph.go
@@ -0,0 +1,65 @@
+package main
+
+import "fmt"
+
+// Edge represents an edge between two vertices
+type Edge struct {
+	src  string
+	dest string
+}
+
+var graph map[string][]string
+var uniqueEdges []Edge
+
+func init() {
+	graph = make(map[string][]string)
+	uniqueEdges = make([]Edge, 0)
+}
+
+func addVertexToGraph(vtx string) {
+	if graph[vtx] != nil {
+		fmt.Println("\n-- Vertex already exists. --")
+		return
+	}
+	vertices[vtx] = len(vertices)
+	graph[vtx] = make([]string, 0)
+}
+
+func addEdgeToGraph(fromVtx, toVtx string) {
+	if graph[fromVtx] == nil { // check if initial vertex exists
+		fmt.Println("\n-- Initial vertex " + fromVtx + " does not exist. --")
+		return
+	}
+	for i := range graph[fromVtx] { // check if edge already exists
+		if graph[fromVtx][i] == toVtx {
+			fmt.Println("\n-- Edge between " + fromVtx + " and " + toVtx + " already exists. --")
+			return
+		}
+	}
+	if graph[toVtx] == nil { // create new destination vertext if it does not exists
+		graph[toVtx] = make([]string, 0)
+		vertices[toVtx] = len(vertices)
+		fmt.Println("\n-- Destination vertex " + toVtx + " created. --")
+	}
+
+	graph[fromVtx] = append(graph[fromVtx], toVtx)
+	graph[toVtx] = append(graph[toVtx], fromVtx)
+	uniqueEdges = append(uniqueEdges, Edge{src: fromVtx, dest: toVtx})
+	return
+}
+
+func addVertex() {
+	var vtxName string
+	fmt.Print("Enter the name of vertex: ")
+	fmt.Scanf("%s", &vtxName)
+	addVertexToGraph(vtxName)
+}
+
+func addEdge() {
+	var fromVtx, toVtx string
+	fmt.Print("Enter the initial vertex name: ")
+	fmt.Scanf("%s", &fromVtx)
+	fmt.Print("Enter the destination vertex name: ")
+	fmt.Scanf("%s", &toVtx)
+	addEdgeToGraph(fromVtx, toVtx)
+}
---
+++ algorithms/union_find/union_find.go
@@ -0,0 +1,117 @@
+package main
+
+import "fmt"
+
+// UnionFind run union find algorithm and detects cycle in give graph
+func UnionFind() {
+	flag := 0
+	for i := 0; i < len(uniqueEdges); i = i + 1 {
+		x := find(uniqueEdges[i].src)
+		y := find(uniqueEdges[i].dest)
+		if x == y {
+			fmt.Println("\n-- Cycle FOUND between:", uniqueEdges[i].src, uniqueEdges[i].dest, ". --")
+			flag = 1
+		} else {
+			union(x, y)
+		}
+	}
+	if flag == 0 {
+		fmt.Println("\n-- NO cycle found in graph. --")
+	}
+}
+
+func find(i string) string {
+	if parent[i] == "-1" {
+		return i
+	}
+	return find(parent[i])
+}
+
+func union(vtx1, vtx2 string) {
+	xset := find(vtx1)
+	yset := find(vtx2)
+	parent[xset] = yset
+}
+
+var parent map[string]string
+var vertices map[string]int
+
+func init() {
+	vertices = make(map[string]int)
+	parent = make(map[string]string)
+}
+
+func main() {
+	fmt.Println("\n-- Union Find --")
+	i := 0
+	for i == 0 {
+		fmt.Println("\n1. ADD A VERTEX")
+		fmt.Println("2. ADD AN EDGE")
+		fmt.Println("3. RUN UNION FIND")
+		fmt.Println("4. EXIT")
+		var choice int
+		fmt.Print("Enter your choice: ")
+		fmt.Scanf("%d", &choice)
+		switch choice {
+		case 1:
+			addVertex()
+			break
+		case 2:
+			addEdge()
+			break
+		case 3:
+			UnionFind()
+		case 4:
+			i = 1
+			break
+		default:
+			fmt.Println("Command not recognized.")
+		}
+	}
+}
+
+func startUnionFind() {
+	// for i := range uniqueEdges {
+	// 	fmt.Println(i, uniqueEdges[i])
+	// }
+	for i := range vertices {
+		parent[i] = "-1"
+	}
+	UnionFind()
+}
+
+// addVertexToGraph("0")
+// addVertexToGraph("1")
+// addVertexToGraph("2")
+// addVertexToGraph("3")
+// addVertexToGraph("4")
+// addEdgeToGraph("0", "1")
+// addEdgeToGraph("0", "2")
+// addEdgeToGraph("1", "3")
+// addEdgeToGraph("1", "4")
+// addEdgeToGraph("3", "4")
+
+// addVertexToGraph("1")
+// addVertexToGraph("2")
+// addVertexToGraph("3")
+// addVertexToGraph("4")
+// addVertexToGraph("5")
+// addVertexToGraph("6")
+// addVertexToGraph("7")
+// addVertexToGraph("8")
+// addEdgeToGraph("1", "3")
+// addEdgeToGraph("1", "2")
+// addEdgeToGraph("2", "4")
+// addEdgeToGraph("3", "4")
+// addEdgeToGraph("2", "5")
+// addEdgeToGraph("5", "7")
+// addEdgeToGraph("5", "6")
+// addEdgeToGraph("8", "6")
+// addEdgeToGraph("8", "7")
+
+// addVertexToGraph("0")
+// addVertexToGraph("1")
+// addVertexToGraph("2")
+// addEdgeToGraph("0", "1")
+// addEdgeToGraph("1", "2")
+// addEdgeToGraph("0", "2")
