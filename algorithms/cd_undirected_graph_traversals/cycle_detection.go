f49c246f71cc2a2a08c6b7732f24aa080611b5fd
computer0101 <deveshpatel7643@gmail.com>
2019-01-28 16:52:10 +0530

    implement cycle detection in undirected graph using dfs

---
+++ algorithms/cd_undirected_graph_traversals/cycle_detection.go
@@ -0,0 +1,106 @@
+package main
+
+import "fmt"
+
+func main() {
+	g := make(graph)
+
+	i := 0
+	for i == 0 {
+		fmt.Println("\n1. ADD A VERTEX")
+		fmt.Println("2. ADD AN EDGE")
+		fmt.Println("3. DETECT CYCLE USING DFS")
+		fmt.Println("4. DETECT CYCLE USING BFS")
+		fmt.Println("5. SIMPLE DISPLAY")
+		fmt.Println("6. EXIT")
+		var choice int
+		fmt.Print("Enter your choice: ")
+		fmt.Scanf("%d", &choice)
+		switch choice {
+		case 1:
+			g.addVertex()
+			break
+		case 2:
+			g.addEdge()
+			break
+		case 3:
+			result, cycle := g.detectDFS()
+			if cycle {
+				fmt.Println("\n-- Given graph is cyclic. --")
+			} else if result != nil && !cycle {
+				fmt.Println("\n-- Given graph is acyclic. --")
+			}
+			break
+		case 4:
+			result := g.detectBFS()
+			fmt.Println(result)
+			break
+		case 5:
+			g.simpleDisplay()
+			break
+		case 6:
+			i = 1
+			break
+		default:
+			fmt.Println("Command not recognized.")
+		}
+	}
+}
+
+func (g graph) simpleDisplay() {
+	fmt.Println("")
+	for i := range g {
+		fmt.Print(i, " => ")
+		for j := range g[i] {
+			fmt.Print(g[i][j] + " ")
+		}
+		fmt.Println("")
+	}
+}
+
+// cyclic
+// g.addVertexToGraph("0")
+// g.addVertexToGraph("1")
+// g.addVertexToGraph("2")
+// g.addVertexToGraph("3")
+// g.addVertexToGraph("4")
+// g.addEdgeToGraph("0", "1")
+// g.addEdgeToGraph("1", "2")
+// g.addEdgeToGraph("2", "4")
+// g.addEdgeToGraph("2", "3")
+// g.addEdgeToGraph("3", "0")
+// g.addEdgeToGraph("4", "2")
+
+// acyclic
+// g.addVertexToGraph("0")
+// g.addVertexToGraph("1")
+// g.addVertexToGraph("2")
+// g.addVertexToGraph("3")
+// g.addVertexToGraph("4")
+// g.addEdgeToGraph("0", "1")
+// g.addEdgeToGraph("1", "2")
+// g.addEdgeToGraph("2", "3")
+// g.addEdgeToGraph("4", "2")
+
+// cyclic
+// g.addVertexToGraph("0")
+// g.addVertexToGraph("1")
+// g.addVertexToGraph("2")
+// g.addVertexToGraph("3")
+// g.addVertexToGraph("4")
+// g.addEdgeToGraph("0", "1")
+// g.addEdgeToGraph("0", "2")
+// g.addEdgeToGraph("2", "3")
+// g.addEdgeToGraph("2", "4")
+// g.addEdgeToGraph("3", "4")
+
+// acyclic
+// g.addVertexToGraph("0")
+// g.addVertexToGraph("1")
+// g.addVertexToGraph("2")
+// g.addVertexToGraph("3")
+// g.addVertexToGraph("4")
+// g.addEdgeToGraph("0", "1")
+// g.addEdgeToGraph("0", "2")
+// g.addEdgeToGraph("2", "3")
+// g.addEdgeToGraph("2", "4")
---
+++ algorithms/cd_undirected_graph_traversals/traversals.go
@@ -0,0 +1,81 @@
+package main
+
+import "fmt"
+
+// detectDFS traverses a graph using dfs technique to detect cycle in a graph
+func (g graph) detectDFS() ([]string, bool) {
+	visited := make(map[string]bool)
+	cycle := false
+	var result []string
+	if len(g) == 0 {
+		fmt.Println("\n-- Graph is empty. --")
+		return nil, false
+	}
+	for i := range g {
+		if !visited[i] {
+			result, cycle = g.dfsHelper(i, visited, result, "-1")
+			if cycle {
+				return nil, cycle
+			}
+		}
+	}
+	return result, cycle
+}
+
+// dfsHelper recursively calls itself to solve dfs traversal
+func (g graph) dfsHelper(vtx string, visited map[string]bool, result []string, parent string) ([]string, bool) {
+	visited[vtx] = true
+	cycle := false
+	result = append(result, vtx)
+	for i := range g[vtx] {
+		fmt.Println(parent, vtx, g[vtx][i])
+		if parent != g[vtx][i] && parent != "-1" && visited[g[vtx][i]] { // cycle detection logic here
+			return nil, true
+		}
+		if !visited[g[vtx][i]] {
+			result, cycle = g.dfsHelper(g[vtx][i], visited, result, vtx)
+			return result, cycle
+		}
+	}
+	return result, cycle
+}
+
+// TODO:
+// detectBFS traverses a graph using dfs technique to detect cycle in a graph
+func (g graph) detectBFS() []string {
+
+	if len(g) == 0 {
+		fmt.Println("\n-- Graph is empty. --")
+		return nil
+	}
+
+	visited := make(map[string]bool)
+
+	var queue []string
+	var result []string
+	var currentVtx string
+
+	for i := range g {
+		if !visited[i] {
+			visited[i] = true
+			queue = append(queue, i)
+
+			for len(queue) != 0 {
+				currentVtx = queue[0]               // get the element from the queue
+				queue = queue[1:len(queue)]         // remove element from the queue
+				result = append(result, currentVtx) // push the current vertex in result
+
+				for i := range g[currentVtx] {
+					if !visited[g[currentVtx][i]] {
+						visited[g[currentVtx][i]] = true        // mark as visited
+						queue = append(queue, g[currentVtx][i]) // push neighbors of current vertex in the queue
+					}
+				}
+
+			}
+
+		}
+	}
+
+	return result
+}
