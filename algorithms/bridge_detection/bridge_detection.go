82a165da17c03813088109f1aa2ed760c96809ab
computer0101 <deveshpatel7643@gmail.com>
2019-01-30 04:25:33 +0530

    implement bridge detection

---
+++ algorithms/bridge_detection/bridge_detection.go
@@ -0,0 +1,69 @@
+package main
+
+import "fmt"
+
+func main() {
+	g := make(graph)
+	i := 0
+	for i == 0 {
+		fmt.Println("\n1. ADD A VERTEX")
+		fmt.Println("2. ADD AN EDGE")
+		fmt.Println("3. DETECT BRIDGES")
+		fmt.Println("4. SIMPLE DISPLAY")
+		fmt.Println("5. EXIT")
+		var choice int
+		fmt.Print("Enter your choice: ")
+		fmt.Scanf("%d", &choice)
+		switch choice {
+		case 1:
+			g.addVertex()
+			break
+		case 2:
+			g.addEdge()
+			break
+		case 3:
+			result := g.findBridges()
+			prev := ""
+			if len(result) != 0 {
+				fmt.Println("\n-- Bridge/s exists in a given graph. --")
+				for i := range result {
+					if prev != "" {
+						fmt.Println("-- Bridge exists between:", prev, "-", result[i])
+						prev = ""
+					} else {
+						prev = result[i]
+					}
+				}
+			} else {
+				fmt.Println("\n -- No bridge exists in a given graph. --")
+			}
+			break
+		case 4:
+			g.simpleDisplay()
+			break
+		default:
+			fmt.Println("Command not recognized.")
+		}
+	}
+}
+
+// c-d, c-h, h-i
+// g.addVertexToGraph("a")
+// g.addVertexToGraph("b")
+// g.addVertexToGraph("c")
+// g.addVertexToGraph("d")
+// g.addVertexToGraph("e")
+// g.addVertexToGraph("f")
+// g.addVertexToGraph("g")
+// g.addVertexToGraph("h")
+// g.addVertexToGraph("i")
+// g.addEdgeToGraph("a", "b")
+// g.addEdgeToGraph("b", "c")
+// g.addEdgeToGraph("c", "a")
+// g.addEdgeToGraph("c", "d")
+// g.addEdgeToGraph("d", "e")
+// g.addEdgeToGraph("e", "f")
+// g.addEdgeToGraph("f", "g")
+// g.addEdgeToGraph("g", "d")
+// g.addEdgeToGraph("c", "h")
+// g.addEdgeToGraph("h", "i")
---
+++ algorithms/bridge_detection/traversal.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+var id float64
+var ids map[string]float64
+var low map[string]float64
+
+func init() {
+	id = 0
+	ids = make(map[string]float64)
+	low = make(map[string]float64)
+}
+
+// DFS traverses a graph using dfs technique
+func (g graph) findBridges() []string {
+	visited := make(map[string]bool)
+	bridges := make([]string, 0)
+	if len(g) == 0 {
+		fmt.Println("\n-- Graph is empty. --")
+		return nil
+	}
+	for i := range g {
+		if !visited[i] {
+			bridges = g.dfs(i, visited, "-1", bridges)
+		}
+	}
+	return bridges
+}
+
+// dfsHelper recursively calls itself to solve dfs traversal
+func (g graph) dfs(at string, visited map[string]bool, parent string, bridges []string) []string {
+	visited[at] = true
+	id = id + 1
+	low[at] = id
+	ids[at] = id
+	for i := range g[at] {
+		to := g[at][i]
+		if to == parent {
+			continue
+		}
+		if !visited[to] {
+			bridges = g.dfs(to, visited, at, bridges)
+			low[at] = math.Min(low[at], low[to])
+			if ids[at] < low[to] {
+				fmt.Println("Entered", at, to)
+				bridges = append(bridges, at)
+				bridges = append(bridges, to)
+			}
+		} else {
+			low[at] = math.Min(low[at], ids[to])
+		}
+	}
+	return bridges
+}
+
+func (g graph) simpleDisplay() {
+	fmt.Println("")
+	for i := range g {
+		fmt.Print(i, " => ")
+		for j := range g[i] {
+			fmt.Print(g[i][j] + " ")
+		}
+		fmt.Println("")
+	}
+}
