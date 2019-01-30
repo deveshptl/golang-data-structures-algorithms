96d9201e69288cb1362ea6d0bb5761b6fa5e20b1
computer0101 <deveshpatel7643@gmail.com>
2019-01-31 02:51:11 +0530

    implement detection of articulation points in an undirected graph

---
+++ algorithms/articulation_point_detection/graph.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+type graph map[string][]string
+
+func (g graph) addVertexToGraph(vtx string) {
+	if g[vtx] != nil {
+		fmt.Println("\n-- Vertex already exists. --")
+		return
+	}
+	g[vtx] = make([]string, 0)
+}
+
+func (g graph) addEdgeToGraph(fromVtx, toVtx string) {
+	if g[fromVtx] == nil { // check if initial vertex exists
+		fmt.Println("\n-- Initial vertex " + fromVtx + " does not exist. --")
+		return
+	}
+	for i := range g[fromVtx] { // check if edge already exists
+		if g[fromVtx][i] == toVtx {
+			fmt.Println("\n-- Edge between " + fromVtx + " and " + toVtx + " already exists. --")
+			return
+		}
+	}
+	if g[toVtx] == nil { // create new destination vertext if it does not exists
+		g[toVtx] = make([]string, 0)
+		fmt.Println("\n-- Destination vertex " + toVtx + " created. --")
+	}
+
+	g[fromVtx] = append(g[fromVtx], toVtx)
+	g[toVtx] = append(g[toVtx], fromVtx)
+	return
+}
+
+func (g graph) addVertex() {
+	var vtxName string
+	fmt.Print("Enter the name of vertex: ")
+	fmt.Scanf("%s", &vtxName)
+	g.addVertexToGraph(vtxName)
+}
+
+func (g graph) addEdge() {
+	var fromVtx, toVtx string
+	fmt.Print("Enter the initial vertex name: ")
+	fmt.Scanf("%s", &fromVtx)
+	fmt.Print("Enter the destination vertex name: ")
+	fmt.Scanf("%s", &toVtx)
+	g.addEdgeToGraph(fromVtx, toVtx)
+}
---
+++ algorithms/articulation_point_detection/traversal.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+var id float64
+var outEdgeCount float64
+var ids map[string]float64
+var low map[string]float64
+var isArt map[string]bool
+
+func init() {
+	id = 0
+	outEdgeCount = 0
+	ids = make(map[string]float64)
+	low = make(map[string]float64)
+	isArt = make(map[string]bool)
+}
+
+// DFS traverses a graph using dfs technique
+func (g graph) findArticulationPoints() map[string]bool {
+	visited := make(map[string]bool)
+	if len(g) == 0 {
+		fmt.Println("\n-- Graph is empty. --")
+		return nil
+	}
+	for i := range g {
+		if !visited[i] {
+			outEdgeCount = 0
+			g.dfs(i, visited, i, "-1")
+			isArt[i] = outEdgeCount > 1
+		}
+	}
+	return isArt
+}
+
+// dfsHelper recursively calls itself to solve dfs traversal
+func (g graph) dfs(root string, visited map[string]bool, at string, parent string) {
+	visited[at] = true
+	id = id + 1
+	low[at] = id
+	ids[at] = id
+	for i := range g[at] {
+		to := g[at][i]
+		if to == parent {
+			continue
+		}
+		if !visited[to] {
+			g.dfs(root, visited, to, at)
+			low[at] = math.Min(low[at], low[to])
+			if ids[at] < low[to] {
+				isArt[at] = true
+			}
+			if ids[at] == low[to] {
+				isArt[at] = true
+			}
+		} else {
+			low[at] = math.Min(low[at], ids[to])
+		}
+	}
+}
+
+func (g graph) simpleDisplay() {
+	fmt.Println("")
+	for i := range g {
+		fmt.Print(i, " => ")
+		for j := range g[i] {
+			fmt.Print(g[i][j] + " ")
+		}
+		fmt.Println("")
+	}
+}
