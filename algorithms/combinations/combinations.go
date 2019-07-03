22a6890223e4ea4f40d00a9945f588cd0c5e7df8
computer0101 <deveshpatel7643@gmail.com>
2019-07-03 18:30:03 +0530

    implement nCr combinations algorithm

---
+++ algorithms/combinations/combinations.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("\n-- n C r Combinations --")
+
+	var n, r int
+	arr := make([]string, 0)
+
+	fmt.Print("\nEnter the value of n:")
+	fmt.Scanf("%d", &n)
+	fmt.Println("Start entering unique values of n:")
+	for i := 0; i < n; i++ {
+		var temp string
+		fmt.Scanf("%s", &temp)
+		arr = append(arr, temp)
+	}
+	fmt.Print("Enter the value of r: ")
+	fmt.Scanf("%d", &r)
+
+	data := make([]string, r)
+
+	Combinations(arr, data, 0, n-1, 0, r)
+}
+
+// Combinations prints all possible nCr combinations of given array
+func Combinations(arr, data []string, start, end, index, r int) {
+	if index == r {
+		fmt.Println(data)
+		return
+	}
+
+	for i := start; i <= end && end-i+1 >= r-index; i++ {
+		data[index] = arr[i]
+		Combinations(arr, data, i+1, end, index+1, r)
+	}
+
+}
